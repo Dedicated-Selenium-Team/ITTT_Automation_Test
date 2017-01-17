184570abb63d335cdd6f35dbb3dea154ef6bb294
Nilesh <nilesh.vidhate.prdxn@gmail.com>
2017-01-17 16:42:10 +0530

    Space added between : and  #

--- resources/views/cron/escalation.blade.php
+++ resources/views/cron/escalation.blade.php
@@ -6,11 +6,11 @@
 <body>
 ITTT Escalation Report<br><br>
 <?php
-echo "Today's date:".date('d-m-Y')."<br><br>";
-echo "How many users submitted Timesheets:".$escalation_report["timesheet_for_today"]."<br><br>";
-echo "How many total users on the platform:".$escalation_report["total_user"]."<br><br>";
-echo "How many users submitted Timesheets for 6+ hours:".$escalation_report["efficient_user_count"]."<br><br>";
-echo 'How many projects have gone "beyond" their total estimated hours (for all designations) till-date:'.$escalation_report["beyond_estimate"]."<br><br>";
+echo "Today's date: ".date('d-m-Y')."<br><br>";
+echo "How many users submitted Timesheets: ".$escalation_report["timesheet_for_today"]."<br><br>";
+echo "How many total users on the platform: ".$escalation_report["total_user"]."<br><br>";
+echo "How many users submitted Timesheets for 6+ hours: ".$escalation_report["efficient_user_count"]."<br><br>";
+echo 'How many projects have gone "beyond" their total estimated hours (for all designations) till-date: '.$escalation_report["beyond_estimate"]."<br><br>";
 
 ?>
 </body>

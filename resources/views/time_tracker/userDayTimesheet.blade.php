b3122451f4dd2ad58c5514e131a8653add1d74b4
Nilesh <nilesh.vidhate.prdxn@gmail.com>
2017-02-07 11:36:38 +0530

    Changes in usertimesheet view

--- resources/views/time_tracker/userDayTimesheet.blade.php
+++ resources/views/time_tracker/userDayTimesheet.blade.php
@@ -162,7 +162,7 @@
      <tr id="time{{$today_project->id}}">
       <td class="break-words">
         <h3><span class="project_name">{{$today_project->project_name}}</span> - <span class="project_designation">{{$today_project->designation_name}}</span></h3>
-        <p>{{$today_project->comments}}</p>
+        <p><?php echo $today_project->comments;?></p>
       </td>
       <td>
         {{$today_project->hrs_locked}}

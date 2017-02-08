57b44bca4467c3bab42cac359a0505c49cf4ed5b
Vrushali <vrushali.shelar.prdxn@gmail.com>
2017-02-08 11:54:38 +0530

    allow manual entry of date on add new user popup and remove the modal-error-off class on myself page

--- public/js/retrive.js
+++ public/js/retrive.js
@@ -304,7 +304,7 @@ $( document ).ready(function() {
   }
   , 'joining_date': {
     'required': true
-    , 'regex': /^[^?<>*]*$/
+    , 'regX': /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
   }
   , 'client_name': {
     'required': true,
@@ -395,7 +395,8 @@ var errorMessage = {
     , 'dropdown': 'Please select role'
   }
   ,'joining_date': {
-    'required': 'Please select the date and time'
+    'required': 'Please select the date'
+    ,'regX': 'Please enter a valid date'
   }
   , 'client_name': {
     'required': 'Please enter client name',
